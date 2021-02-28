bff7991faec663a3536d6d028d9c3991b2e3ec81
garrett-price <priceg@reed.edu>
2021-02-28 11:37:58 -0800

    performed light wrangling (YEAR from dbl to fct)

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,9 @@
 ## code to prepare `DATASET` dataset goes here
 
-usethis::use_data(DATASET, overwrite = TRUE)
+weed <- read.csv("~/math241/project 1/pkgGrpm/data-raw/weed.csv")
+
+weed$YEAR <- as.factor(weed$YEAR)
+
+
+usethis::use_data(weed, overwrite = TRUE)
+
