8b5f149d22001ffbde08d5260a4103d20f5a8a0f
garrett-price <priceg@reed.edu>
2021-03-01 14:25:15 -0800

    2:25 3/1

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -2,27 +2,31 @@
 
 library(tidyverse)
 
+#load in csv
 weed <- read.csv("~/math241/project 1/pkgGrpm/data-raw/weed.csv")
 
+#turn YEAR and MONTH vars into factors
 weed$YEAR <- as.factor(weed$YEAR)
 
 weed$MONTH <- as.factor(weed$MONTH)
 
-#weed <- weed %>% arrange(match(MONTH, month.name))
+#trim spaces from MONTH
+weed$MONTH <- trimws(weed$MONTH, which = c("both"))
 
+#reorder MONTH so it reflects the calendar order
 weed <- weed %>% 
-  mutate(MONTH = fct_relevel(MONTH, levels = c("JANUARY  ", 
-                                               "FEBRUARY ", 
-                                               "MARCH    ", 
-                                               "APRIL    ", 
-                                               "MAY      ",
-                                               "JUNE     ",
-                                               "JULY     ", 
-                                               "AUGUST   ", 
+  mutate(MONTH = fct_relevel(MONTH, levels = c("JANUARY", 
+                                               "FEBRUARY", 
+                                               "MARCH", 
+                                               "APRIL", 
+                                               "MAY",
+                                               "JUNE",
+                                               "JULY", 
+                                               "AUGUST", 
                                                "SEPTEMBER",
-                                               "OCTOBER  ",
-                                               "NOVEMBER ", 
-                                                "DECEMBER ")))
+                                               "OCTOBER",
+                                               "NOVEMBER", 
+                                                "DECEMBER")))
 
 usethis::use_data(weed, overwrite = TRUE)
 
