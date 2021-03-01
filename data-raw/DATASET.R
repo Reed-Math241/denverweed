eccca73b2dc1d0bcdff4ad38edca62a1e59bca6a
garrett-price <priceg@reed.edu>
2021-03-01 14:12:15 -0800

    2:12 3/1

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -6,8 +6,23 @@ weed <- read.csv("~/math241/project 1/pkgGrpm/data-raw/weed.csv")
 
 weed$YEAR <- as.factor(weed$YEAR)
 
-weed <- weed %>% arrange(match(MONTH, month.name))
+weed$MONTH <- as.factor(weed$MONTH)
 
+#weed <- weed %>% arrange(match(MONTH, month.name))
+
+weed <- weed %>% 
+  mutate(MONTH = fct_relevel(MONTH, levels = c("JANUARY  ", 
+                                               "FEBRUARY ", 
+                                               "MARCH    ", 
+                                               "APRIL    ", 
+                                               "MAY      ",
+                                               "JUNE     ",
+                                               "JULY     ", 
+                                               "AUGUST   ", 
+                                               "SEPTEMBER",
+                                               "OCTOBER  ",
+                                               "NOVEMBER ", 
+                                                "DECEMBER ")))
 
 usethis::use_data(weed, overwrite = TRUE)
 
